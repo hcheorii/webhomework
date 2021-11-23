f2aa623c45003a32bcc6dd63bc7fcb5ff6c1b61e
hcheorii <zkxmzhrhtn@naver.com>
2021-11-24 02:55:56 +0900

    test

---
+++ wkqk.js
@@ -0,0 +1,10 @@
+function moveUrl(){
+    var country = document.getElementById("inputName").value;
+    if (country == "몽골"){
+        window.open('http://www.daum.net ', '_blank'); 
+    }
+
+
+
+
+}
